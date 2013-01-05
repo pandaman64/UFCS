67ed639590e1aa53e10f73d02c40e36255ddac61
pandaman64 <kointosudesuyo@infoseek.jp>
2013-01-05 17:56:32 +0900

    Update main.cpp
    不要#includeの削除

--- main.cpp
+++ main.cpp
@@ -6,11 +6,6 @@
 #include <iterator>
 #include <algorithm>
 
-#include "algorithm.h"
-#include "make_ufcs_wrapper.h"
-#include "boost_range_adapt.h"
-#include "is_callable.h"
-
 struct map_impl{
 	template<typename T,typename F>
 	typename std::remove_reference<T>::type operator ()(T && seq,F &&f){
@@ -32,11 +27,6 @@ struct print_impl{
 	}
 };
 MAKE_UFCS_WRAPPER(print_impl,print);
-
-struct hoge{
-	void operator ()(){
-	}
-};
 
 int main(){
 	auto l = {1,2,3,4,5,6};
